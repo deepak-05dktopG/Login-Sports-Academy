59ccfad5fcd8a96ef85377ae73f1c5faacbaaa6c
Deepakkumar <kumardeepak59422@gmail.com>
2026-03-28 21:06:57 +0530

    mail update

--- client/src/content/brand.js
+++ client/src/content/brand.js
@@ -2,7 +2,7 @@ export const BRAND = {
   name: 'Login Sports Academy',
   shortName: 'Login Swim',
   tagline: 'Confident swimmers. Strong technique. Safer water.',
-  email: 'loginsportsacadamy@gmail.com',
+  email: 'loginsportsacademy6@gmail.com',
   phonePrimary: '+91 9952139201',
   phoneSecondary: '+91 9952169201',
   phoneTertiary: '+91 9865068086',
